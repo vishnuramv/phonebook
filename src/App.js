3a855f159d55c1a00e4561050e0d4a580035b04c
Vishnu Ram V <vishnu.v1902@gmail.com>
2022-09-24 01:43:27 +0530

    template initialized

--- src/App.js
+++ src/App.js
@@ -1,60 +1,10 @@
-import { Box } from '@mui/material';
-import { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { getUser } from './apiClient/userApi';
-import './App.css';
-import ContactList from './components/ContactList';
-import Header from './components/Header';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import { userStore } from './store';
+import { Typography } from "@mui/material";
 
 function App() {
-  const isLoggedIn = userStore.useState(s => s.isLoggedIn);
-  useEffect(() => {
-    const token = localStorage.getItem("token")
-    if (!!token) {
-      userStore.update(s => {
-        s.isLoggedIn = true
-        s.token = token
-      })
-      getUser().then(res => {
-        userStore.update(s => {
-          s.user = res
-        })
-        console.log(res)
-      }).catch(err => {
-        console.log(err)
-      })
-    }
-  }, [])
-
 
   return (
     <div className="App">
-      <Routes>
-        {/* <Route path="/" element={<h1>hh</h1>} />
-        <Route path="/" element={<h1>h</h1>} /> */}
-        {
-          !isLoggedIn ? (
-            <>
-              <Route path="/" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-            </>
-          ) : (
-            <>
-              <Route path="/" element={
-                <>
-                  <Header />
-                  <Box px={17} py={3}>
-                    <ContactList />
-                  </Box>
-                </>
-              } />
-            </>
-          )
-        }
-      </Routes>
+      <Typography align="center">Hello world🚀!</Typography>
     </div>
   );
 }
